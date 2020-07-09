bcc8959e92162d5ec414c59e2f5fd268198c9215
王元帅 <wangyuanshuai@jupin.net.cn>
2020-07-09 09:24:04 +0800

    Update save_sms_to_mongo.py

--- save_sms_to_mongo.py
+++ save_sms_to_mongo.py
@@ -6,11 +6,16 @@ from pymongo import MongoClient
 
 def save_sms(argv):
     try:
-        client = MongoClient('mongodb://192.168.1.210/zz_web')
-        db = client.zz_web
-        zz_sms = db.zz_sms
-        post_id = zz_sms.insert_one(argv).inserted_id
-        print(post_id)
+        client_test = MongoClient('mongodb://192.168.1.210/zz_web')
+        db_test = client_test.zz_web
+        zz_sms_test = db_test.zz_sms
+        post_id_test = zz_sms_test.insert_one(argv).inserted_id
+        print(post_id_test)
+        client_pro = MongoClient('mongodb://192.168.1.210/zz_web')
+        db_pro = client_pro.zz_web
+        zz_sms_pro = db_pro.zz_sms
+        post_id_pro = zz_sms_pro.insert_one(argv).inserted_id
+        print(post_id_pro)
     except Exception as e:
         print(e)
     else:
